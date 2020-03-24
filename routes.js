cdb10abd8b17ee66f6d19fbce87753e58766835e
Rohan Kalsi <rohankalsi@Rohans-MacBook-Pro.local>
2020-03-24 15:14:57 +0530

    Added more routes and pugs

--- routes.js
+++ routes.js
@@ -5,4 +5,20 @@ router.get('/', (req,res) => {
     res.render('index')
 })
 
+router.get('/found-form', (req,res) => {
+    res.render('found-form')
+}) 
+
+router.get('/lost-form', (req,res) => {
+    res.render('lost-form')
+})
+
+router.get('/lost',(req,res) => {
+    res.render('lost')
+})
+
+router.get('/found', (req,res) => {
+    res.render('found')
+})
+
 module.exports = router
