28b816cab0b8c33fa611e81fc3ab6edf10a60660
Rohan Kalsi <rohankalsi@Rohans-MacBook-Pro.local>
2020-03-24 14:46:42 +0530

    Routes added successfully

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express()
 const port = 3010
 const path = require('path')
+const routes = require('./routes')
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+app.use(routes)
 
 app.listen(port, () => {console.log(`You are now listening on port: ${port}`)} )
