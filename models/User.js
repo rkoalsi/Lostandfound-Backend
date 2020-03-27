86ec516280a56e05f1e5627843da67477709c4cc
Rohan Kalsi <rohankalsi@Rohans-MacBook-Pro.local>
2020-03-28 02:00:38 +0530

    Added Passport,bcrypt,session etc

--- models/User.js
+++ models/User.js
@@ -1,23 +1,18 @@
 const mongoose = require('mongoose');
-require('mongoose-type-email');
 const mongooseTypePhone = require('mongoose-type-phone');
 
-mongoose.SchemaTypes.Email.defaults.message = 'Email address is invalid';
-
 const UserSchema = new mongoose.Schema({
-  name: { type: 'string', required: true },
-  email: { email: mongoose.SchemaTypes.Email },
+  name: {
+    type: 'string',
+    required: true
+  },
+  email: {
+    type: 'string',
+    required: true
+  },
   phone: {
-    type: mongoose.SchemaTypes.Phone,
-    required: 'Phone number should be 10 digits',
-    allowBlank: false,
-    allowedNumberTypes: [
-      mongooseTypePhone.PhoneNumberType.MOBILE,
-      mongooseTypePhone.PhoneNumberType.FIXED_LINE_OR_MOBILE
-    ],
-    phoneNumberFormat: mongooseTypePhone.PhoneNumberFormat.INTERNATIONAL,
-    defaultRegion: 'IN',
-    parseOnGet: false
+    type: "number",
+    required: true
   },
   date: {
     type: Date,
@@ -25,5 +20,5 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
-const User = mongoose.Model('User', UserSchema);
+const User = mongoose.model('User', UserSchema);
 module.exports = User;
--- routes.js
+++ routes.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
+const passport = require('passport')
+const User = require('./models/User')
 
 router.get('/', (req, res) => {
     res.render('index')
