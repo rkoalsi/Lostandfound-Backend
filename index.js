cf7cb566c2fb00dba6e54b44c9013f9966db0b36
Rohan Kalsi <rohankalsi@Rohans-MacBook-Pro.local>
2020-03-28 02:08:18 +0530

    Dotenv

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const app = express()
-const port = 3010
+const port = process.env.PORT || 3010;
 const path = require('path')
 const routes = require('./routes')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
-mongoose.connect('mongodb://localhost:27017/Lost&Found', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/Lost&Found', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 app.engine('ejs', require('ejs').__express)
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 app.use(bodyParser.json())
 app.use(routes)
 
-app.listen(port, () => {console.log(`You are now listening on port: ${port}`)} )
+app.listen(port, () => {
+    console.log(`You are now listening on port: ${port}`)
+})
