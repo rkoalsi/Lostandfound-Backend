65fac108e31ce32f435956654ce8f9ad735de3db
Rohan Kalsi <rohankalsi@Rohans-MacBook-Pro.local>
2020-03-27 19:46:19 +0530

    added lost and found item routes

--- routes.js
+++ routes.js
@@ -1,26 +1,33 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
     res.render('index')
 })
 
 router.use('/', express.static('./static'));
 
-router.get('/found-form', (req,res) => {
+router.get('/found-form', (req, res) => {
     res.render('found-form')
-}) 
+})
 
-router.get('/lost-form', (req,res) => {
+router.get('/lost-form', (req, res) => {
     res.render('lost-form')
 })
 
-router.get('/lost',(req,res) => {
+router.get('/lost', (req, res) => {
     res.render('lost')
 })
- 
-router.get('/found', (req,res) => {
+
+router.get('/found', (req, res) => {
     res.render('found');
 })
 
+router.get('/found-item', (req, res) => {
+    res.render('found-item')
+})
+
+router.get('/lost-item', (req, res) => {
+    res.render('lost-item')
+})
 module.exports = router
